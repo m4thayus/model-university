440f67f1f8ed059dda3a40122382ff9761bcf7a4
Matt Williams <matt.seongjun@gmail.com>
2019-06-12 13:54:28 -0400

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,5 @@
 Rails.application.routes.draw do
-  get 'courses/index'
-  get 'courses/show'
-  get 'courses/new'
-  get 'courses/edit'
-  get 'professors/index'
-  get 'professors/show'
-  get 'professors/new'
-  get 'professors/edit'
-  get 'students/index'
-  get 'students/show'
-  get 'students/new'
-  get 'students/edit'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    resources :students, :professors, :courses
+    
 end
