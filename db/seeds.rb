d9439ead09d075bfeb09b1afa1a39d666ccfe206
Matt Williams <matt.seongjun@gmail.com>
2019-06-12 14:25:47 -0400

    Add seeds and run migrations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+who = Professor.create(name: "Dr. Who", field: "Time Travel")
+rose = Student.create(name: "Rose Tyler", year: 2005, major: "Companionship")
+history = Course.create(
+    name: "History of the Earth", 
+    description: "Earth from creation to destrucion", 
+    time: "9:30", 
+    professor_id: who.id
+)
+episode2 = Lecture.create(
+    student_id: rose.id,
+    course_id: history.id
+)
